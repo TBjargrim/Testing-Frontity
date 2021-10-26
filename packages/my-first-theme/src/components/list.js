a3456de5c16eb3c28d07dddb183dc39ef3a46ae4
tbjargrim <therese.bjargrim@student.KYH.se>
2021-10-26 13:51:13 +0200

    Style added

--- packages/my-first-theme/src/components/list.js
+++ packages/my-first-theme/src/components/list.js
@@ -1,11 +1,11 @@
 import React from "react"
-import { connect } from "frontity"
+import { connect, styled } from "frontity"
 import Link from "@frontity/components/link"
 
 const List = ({ state }) => {
   const data = state.source.get(state.router.link)
   return (
-    <div>
+    <Items>
     {data.items.map((item) => {
       const post = state.source[item.type][item.id]
       return (
@@ -15,7 +15,17 @@ const List = ({ state }) => {
         </Link>
       )
     })}
-  </div>
+  </Items>
   )
 }
 export default connect(List)
+
+const Items = styled.div`
+  & > a {
+    display: block;
+    margin: 6px 0;
+    font-size: 1.2em;
+    color: steelblue;
+    text-decoration: none;
+  }
+`
